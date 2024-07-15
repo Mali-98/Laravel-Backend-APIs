fe88797e2500dd3327cdbbf0082c017b1b7a6304
mali1998 <mali@arconme.com>
2024-07-15 14:12:08 +0300

    update player controller

--- laravel-crud-api/app/Http/Controllers/playerController.php
+++ laravel-crud-api/app/Http/Controllers/playerController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use app\Models\Player;
+
 class PlayerController extends Controller
 {
     //display a listing of the resource. return json == GET ALL
