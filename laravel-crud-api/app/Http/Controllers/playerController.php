83255c0639708283f12c64d283960c8a25546d48
mali1998 <mali@arconme.com>
2024-07-15 14:30:41 +0300

    update player controller

--- laravel-crud-api/app/Http/Controllers/playerController.php
+++ laravel-crud-api/app/Http/Controllers/playerController.php
@@ -2,25 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Player;
 use Illuminate\Http\Request;
-
-use app\Models\Player;
 
 class PlayerController extends Controller
 {
-    //display a listing of the resource. return json == GET ALL
+    // display a listing of the resource. treturn json
     public function index()
     {
         $players = Player::all();
         return response()->json($players);
     }
 
-    //store a newly created resource in storage ==CREATE
+    // store a newly created resource in storage.
     public function store(Request $request)
     {
         $validatedData = $request->validate([
-            "name" => 'required|string',
-            "email" => 'required|string'
+            'name' => 'required|string',
+            'email' => 'required|string',
         ]);
 
         $player = Player::create($validatedData);
@@ -28,28 +27,30 @@ class PlayerController extends Controller
         return response()->json($player, 201);
     }
 
-    //display the specified resource
+    // display the specified resource.
     public function show(Player $player)
     {
         return response()->json($player);
     }
 
-    //update the specified resource
+    // update the specified resource in storage.
     public function update(Request $request, Player $player)
     {
         $validatedData = $request->validate([
             'name' => 'required|string',
-            'email' => 'required|string'
+            'email' => 'required|string',
         ]);
 
         $player->update($validatedData);
-        return response()->json($player, 0);
+
+        return response()->json($player, 200);
     }
 
-    //remove the specified resource
+    // remove the specified resource from storage.
     public function destroy(Player $player)
     {
         $player->delete();
+
         return response()->json(null, 204);
     }
 }
